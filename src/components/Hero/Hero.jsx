074fc50493799cc04bd5dacda52d60b24e084981
Konovalova-Kateryna <kytana2112@gmail.com>
2023-10-15 21:05:56 +0300

    refactor: rename classnames

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import "./hero.scss";
 
 const Hero = () => {
   return (
-    <section className="hero__container">
-      <div className="hero__tumb">
+    <section className="hero">
+      <div className="hero__container">
         <h2 className="hero__title">
           В цю гру
           <span className="hero__title-unit">грають </span>
