ab7fe14d03efeb1d6c85e13b8c1cdca5b76f6107
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-10-17 22:13:50 +0300

    Revert "Feature header"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,9 @@
-import React from "react";
-import "./header.scss";
-import burgerOpen from "../../assets/header/burger-menu.svg";
+import './header.scss'
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <a className="header-container-logo" href="/index.html">
-        <img
-          className="header-container-logo-img"
-          src="src/assets/header/logo.png"
-          alt="logo"
-          width={88}
-        />
-      </a>
-      <div className="header-container-nav">
-        <ul className="header-container-nav-list">
-          <li className="header-container-nav-list-item">
-            <a className="header-container-nav-list-item-link" href="#corruption">
-              Це корупція
-            </a>
-          </li>
-          <li className="header-container-nav-list-item">
-            <a className="header-container-nav-list-item-link" href="#counteract">
-              Як протидіяти корупції
-            </a>
-          </li>
-        </ul>
-      </div>
-      <a className="header-container-button" href="/">
-        Підтримати проект
-      </a>
+    <div>Header</div>
+  )
+}
 
-      <img src={burgerOpen} alt="Open" className="header-container-burger" />
-    </div>
-  );
-};
-
-export default Header;
+export default Header
