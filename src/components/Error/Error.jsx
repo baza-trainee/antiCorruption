a9d90a38b018f483d475832ae4438776ee72bc23
Nataliia <koreniuknataliia@gmail.com>
2023-10-17 21:59:04 +0300

    feat: added section error

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,26 @@
-import './error.scss'
+import './error.scss';
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div className='404 page not found'>Error</div>
+    <div className='page404'>
+     <div className='page404__content'>
+      <div>
+        <img 
+          className='page404__image'
+          src="/src/assets/error/image-404.png" 
+          alt="404" />
+      </div>
+      <div className='page404__description'>
+        У-у-упс... Сталася помилка - ми не можемо знайти сторінку. Тому пропонуємо Вам повернутись на Головну, поки ми усуваємо несправності
+      </div>
+      <div className='page404__button'>
+        <a className='page404__link'>
+          <Link to='/'>Повернутись на Головну</Link>
+        </a>
+      </div>
+     </div>
+    </div>
   )
 }
 
