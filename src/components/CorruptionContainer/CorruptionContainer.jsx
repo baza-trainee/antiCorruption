c2aa13d186becd624a32938be397791613afa998
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2023-10-17 05:45:43 +0300

    init feature/no-ref/add-corruption-section

--- src/components/CorruptionContainer/CorruptionContainer.jsx
+++ src/components/CorruptionContainer/CorruptionContainer.jsx
@@ -1,7 +1,56 @@
-import "./corruptionContainer.scss";
+import './corruptionContainer.scss';
+import Corruption from '../Corruption/Corruption';
+import { corruptionCases } from '../../utils/constants';
 
 const CorruptionContainer = () => {
-  return <div>CorruptionContainer</div>;
+  return (
+    <section id='home'>
+      <div className='corruption-case'>
+        <h2 className='corruption-case__title'>Це корупція</h2>
+        <ul className='corruption-case__list'>
+          {corruptionCases.map(
+            (
+              {
+                srcCause,
+                srcConseq,
+                textCause,
+                imageCauseDesktop,
+                imageCauseMobileHor,
+                imageCauseTablet,
+                imageCauseMobile,
+                imageConseqDesktop,
+                imageConseqMobileHor,
+                imageConseqTablet,
+                imageConseqMobile,
+                textConsequences,
+                altCause,
+                altConsequences,
+              },
+              index
+            ) => (
+              <Corruption
+                key={index}
+                imageCause={srcCause}
+                imageConsequences={srcConseq}
+                imageCauseDesktop={imageCauseDesktop}
+                imageCauseMobileHor={imageCauseMobileHor}
+                imageCauseTablet={imageCauseTablet}
+                imageCauseMobile={imageCauseMobile}
+                imageConseqDesktop={imageConseqDesktop}
+                imageConseqMobileHor={imageConseqMobileHor}
+                imageConseqTablet={imageConseqTablet}
+                imageConseqMobile={imageConseqMobile}
+                textCause={textCause}
+                textConsequences={textConsequences}
+                altCause={altCause}
+                altConsequences={altConsequences}
+              />
+            )
+          )}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default CorruptionContainer;
